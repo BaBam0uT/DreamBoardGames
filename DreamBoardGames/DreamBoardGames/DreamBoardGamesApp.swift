23d91fbeee00fe4054d9e817ab9b43b4fd928513
Valentin DEDET <valentin.dedet@hotmail.fr>
2022-11-30 15:05:33 +0100

    Create project

---
+++ DreamBoardGames/DreamBoardGames/DreamBoardGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamBoardGamesApp.swift
+//  DreamBoardGames
+//
+//  Created by Valentin Dedet on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct DreamBoardGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
